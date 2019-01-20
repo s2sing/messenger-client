bb0744c929be646ca69f33cb9d80eb730ac55537
JamesGonged <s2sing@naver.com>
2019-01-20 23:55:44 +0900

    로그인 전 단계

--- components/Global.js
+++ components/Global.js
@@ -1,7 +1,24 @@
 function Global() {
 }
 
-Global.version = '1.0.1';
+Global.fs = require('fs');
+
+Global.clientRoot = nw.App.startPath + "\\";
+
 Global.host = 'http://localhost:3000';
 Global.patchUrl = '/versionCheck';
-Global.clientRoot = nw.App.startPath + "\\";
+
+Global.version = Global.fs.readFileSync(Global.clientRoot + 'version', 'utf-8');
+
+// =======================
+// 어플리케이션 초기화
+// =======================
+Global.prototype.init = function(){
+  const win = nw.Window.get();
+  const width = 412;
+  const height = 500;
+  win.resizeTo(width, height);
+  //win.moveTo(screen.width - width, 0);
+  win.setPosition('mouse');
+}
+Global.prototype.init();
--- components/Patch.js
+++ components/Patch.js
@@ -1,37 +1,51 @@
-const http = require('http');
-const fs = require('fs');
-
 function Patch() {
 }
 
+Patch.http = require('http');
+Patch.fs = require('fs');
+
+// ====================
+// 클라이언트 버전 체크
+// ====================
 Patch.prototype.versionCheck = function () {
   $.ajax({url: Global.host + Global.patchUrl})
     .done((res) => {
       if (res.version !== Global.version) {
         res.files.forEach(function (file) {
-          Patch.prototype.fileDownload(Global.host + res.repository + file, file);
+          Patch.prototype.fileDownload(Global.host + res.repository + file, file, res.version);
         });
-      }else{
-
       }
+    })
+    .fail((res) => {
+      alert("서버와 연결에 실패하였습니다.");
     });
 }
 
-Patch.prototype.fileDownload = function (url, file) {
-  http.get(url, function (res) {
-    const fileSize = res.headers['content-length'];
-    res.setEncoding('binary');
+// ====================
+// 클라이언트 다운로드
+// ====================
+Patch.prototype.fileDownload = function (url, file, version) {
+  Patch.http.get(url, function (res) {
+    const {statusCode} = res;
 
-    let a = "";
-    res.on('data', function (chunk) {
-      a += chunk;
-    });
+    if (statusCode === 200) {
+      res.setEncoding('binary');
 
-    res.on('end', function () {
-      fs.writeFile(Global.clientRoot + file, a, 'binary', function (err) {
-        if (err) throw err;
-        document.location.reload(true);
+      let a = "";
+      res.on('data', function (chunk) {
+        a += chunk;
       });
-    });
+
+      res.on('end', function () {
+        Patch.fs.writeFile(Global.clientRoot + file, a, 'binary', function (err) {
+          if (err) throw err;
+          Patch.fs.writeFileSync(Global.clientRoot + 'version', version);
+          alert("클라이언트 버전이 업데이트 되었습니다.\n메신저를 다시 실행시켜 주시기 바랍니다.");
+          nw.App.quit();
+        });
+      });
+    }else{
+      alert("업데이트 요청이 거부되었습니다.");
+    }
   });
 }
